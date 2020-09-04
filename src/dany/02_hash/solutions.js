5e1ffb14c7331cd1f539e7a20b89509209f8f174
devdany <dayong7387@hanmail.net>
2020-09-04 21:04:12 +0900

    review approve

--- src/dany/02_hash/solutions.js
+++ src/dany/02_hash/solutions.js
@@ -1,7 +1,5 @@
 const getBigger2PlayCnt = (playCnts) => {
-  playCnts.sort((a, b) => {
-    return a.cnt < b.cnt ? 1 : a.cnt > b.cnt ? -1 : 0
-  })
+  playCnts.sort((a, b) => b.cnt - a.cnt)
   return [playCnts[0], playCnts[1]]
 }
 
@@ -22,20 +20,9 @@ export const bestAlbum = (genres, plays) => {
     }
   }
   
-  const results = []
-  for (let genre in playPerGenre) {
-    results.push(playPerGenre[genre])
-  }
-
-  results.sort((a, b) => {
-    return a.totalPlayCnt < b.totalPlayCnt ? 1 : a.totalPlayCnt > b.totalPlayCnt ? -1 : 0
-  })
-  
-  let answer = []
-  
-  for (const result of results) {
-    answer = answer.concat(result.playCnts.map((playCnt) => playCnt.id))
-  }
+  const answer = Object.values(playPerGenre)
+    .sort((a, b) => b.totalPlayCnt - a.totalPlayCnt)
+    .reduce((acc, result) => acc.concat(result.playCnts.map(playCnt => playCnt.id)), [])
 
   return answer
 }
