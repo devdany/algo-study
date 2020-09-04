eeede002b24eb24360809aeda35c8693082432d6
devdany <dayong7387@hanmail.net>
2020-09-04 20:08:47 +0900

    hash

--- src/dany/02_hash/02_hash.test.js
+++ src/dany/02_hash/02_hash.test.js
@@ -1,8 +1,12 @@
-import { bestAlbum } from './solutions'
+import { bestAlbum, camouflage } from './solutions'
 
 describe('02_solution_test', () => {
   it('베스트 앨범', () => {
     const result = bestAlbum(['classic', 'pop', 'classic', 'classic', 'pop'], [500, 600, 150, 800, 2500])
     expect(result).toEqual([4, 1, 3, 0])
+  })
+
+  it('위장', () => {
+    expect(camouflage([["y_h", 'head'], ['y_a', 'body']])).toEqual(3)
   })
 })
--- src/dany/02_hash/solutions.js
+++ src/dany/02_hash/solutions.js
@@ -39,3 +39,55 @@ export const bestAlbum = (genres, plays) => {
 
   return answer
 }
+
+const getCombinations = function (arr, selectNumber) {
+  const results = []
+  
+  // 1개씩 택할 때, 바로 모든 배열의 원소 return
+  if (selectNumber === 1) {
+    return arr.map((value) => [value])
+  }
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1) // 해당하는 fixed를 제외한 나머지 뒤
+    const combinations = getCombinations(rest, selectNumber - 1) // 나머지에 대해서 조합을 구한다.
+    const attached = combinations.map((combination) => [fixed, ...combination]) //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+    results.push(...attached) // 배열 spread syntax 로 모두다 push
+  })
+
+  return results
+}
+
+export const camouflage = (clothes) => {
+  if (clothes.length === 0) {
+    return 0
+  }
+  const hash = {}
+  for (const [ name, type ] of clothes) {
+    if (hash[type]) {
+      hash[type] = [...hash[type], name]
+    } else {
+      hash[type] = [name]
+    }
+  }
+
+  const hashArr = []
+  for (const key in hash) {
+    hashArr.push({ type: key, clothes: hash[key] })
+  }
+
+  const typeCnt = Object.keys(hash).length
+  let result = 0
+  for (let i = 1; i <= typeCnt; i++) {
+    // hash 에서 i가지를 고르는 경우
+    const picked = getCombinations(hashArr, i)
+    for (let j = 0; j < picked.length; j++) {
+      let clothCombination = 1
+      for (let k = 0; k < picked[j].length; k++) {
+        clothCombination = clothCombination * picked[j][k].clothes.length
+      }
+      result += clothCombination
+    }
+  }
+  return result
+} 
