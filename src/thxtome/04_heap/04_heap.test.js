0b1bcc3862cbdd1815046e2332ba2b260a02a6ad
thxtome <thxtome531@gmail.com>
2020-09-09 00:16:27 +0900

    dual priority queue solve

--- src/thxtome/04_heap/04_heap.test.js
+++ src/thxtome/04_heap/04_heap.test.js
@@ -1,5 +1,5 @@
 import { diskController } from "./diskController";
-
+import { dualPriorityQueue } from "./dualPriorityQueue";
 describe("diskController", () => {
   it("디스크 컨트롤러", () => {
     const result = diskController([
@@ -8,5 +8,12 @@ describe("diskController", () => {
       [2, 6],
     ]);
     expect(result).toEqual(9);
+  });
+});
+
+describe("dualPriorityQueue", () => {
+  it("이중 우선순위 큐", () => {
+    const result = dualPriorityQueue(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]);
+    expect(result).toEqual([0, 0]);
   });
 });
